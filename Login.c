f00d9758d3497fec2f4aace5ede1f2839afd9685
mikeharbidge <55110294+mikeharbidge@users.noreply.github.com>
2020-03-05 10:26:21 -0700

    more work on login
    Includes an executable that runs on my pc (probably will on yours)

--- Login.c
+++ Login.c
@@ -4,33 +4,80 @@
 
 #include <stdio.h>
 #include <sqlite3.h>
+#include <string.h>
+#include "Login.h"
 
-int loginScreen() {
-//login screen is the parent function for both other 
-//loginScreen returns 1 if login was successful, 0 if failed
+int main()
+{
+    //testing for login
+    int in = 0; //if login succeeded
+    in = loginScreen();
+    if (in)
+        printf("***LOGIN SUCCEEDED***\n");
+    return 0;
+}
 
-int continueTop = 1;
-char userInput = ''; //only a single character is needed as list is numbered
+int loginScreen() 
+{
+    //login screen is the parent function for both other 
+    //loginScreen returns 1 if login was successful, 0 if failed
 
-printf("Welcome to CMPUT 291 Mini-Project 1. Please select a sign-in option from the list below.");
+    int continueTop = 1;
+    char userInput[2]; //only a single character is needed as list is numbered
+    int signInResult = 0; //result of smaller sign-in functions
+
+    printf("Welcome to CMPUT 291 Mini-Project 1. Login screen started.\n");
 
 
-while (continueTop){
-    printf("\n1: Sign in as existing user \n2: Create new user \n3: Quit\nInput: ");
-    scanf("%c", userInput);
-
-    if (userInput == '1')
+    while (continueTop) //top level loop
     {
-        //sign-in with existing
+        printf("Select an option:\n1: Sign in as existing user \n2: Create new user \n3: Quit\nInput: ");
+        scanf("%s", userInput);
+
+        if (userInput[0] == '1')
+        {
+            signInResult = signIn();
+            if (signInResult == 1)
+                return 1; //if sign in failed, return to top loop
+        }
+        else if (userInput[0] == '2')
+        {
+            signInResult = signUp();
+            if (signInResult == 1)
+                return 1;
+        }
+        else if (userInput[0] == '3')
+        {
+            printf("Quitting...\n");
+            return 0;
+        }
+        else
+        {
+            printf("Invalid option. Please select a number from the list.\n");
+        }
+        
     }
-    else if (userInput == '2')
-    {
-        //Create new user
-    }
-    
+    return 0;
+}
 
-    }
+int signIn()
+{
+    //Handles signing in with an existing username and password
+    //if pw incorrect, return to top loop.
+    //will include injection countering later
+    char username[20];
+    char password[20];
+    printf("Enter Username: ");
+    scanf("%s", username);
+    printf("Enter Password: ");
+    scanf("%s", password);
+    printf("%s, %s\n", username,password); //DEBUG
+    if (strcmp(username,"admin") == 0)
+        return 1;
+    return 0;
+}
 
-
-
+int signUp()
+{
+    return 0;
 }
---
+++ Login.h
@@ -0,0 +1,3 @@
+int loginScreen();
+int signIn();
+int signUp();
