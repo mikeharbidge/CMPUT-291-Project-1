56140509b9e2ac09268dc52a417b4b7c244e7131
NickSNFK <61804880+NickSNFK@users.noreply.github.com>
2020-03-05 19:49:52 -0700

    Started CMDLine
    We can move all of it to CMDLine.c  but just for testing purposes, it's in main.c

--- main.c
+++ main.c
@@ -3,16 +3,46 @@
 //Main also includes utility functions for opening and closing SQL databases
 #include "main.h"
 
-int main()
-{
+int main(){
     //testing for login
     //NEEDS TO ERROR CHECK DB INIT
     int in = 0; //if login succeeded
+    int choice;//input for choosing functionality
     in = loginScreen();
-    if (in)
+    if (in){
         printf("***LOGIN SUCCEEDED***\n");
+	printf("1. List Products\n2. Search For Sales\n3. Post a Sale\n4. Search for Users\n5. Logout\n");
+	while(scanf("%d",&choice)){
+	  switch(choice){
+	  case 1:
+	    printf("chose 1\n");
+	    break;
+	    //list products
+	  case 2:
+	    printf("chose 2\n");
+	    break;
+	    //Search for Sales 
+	  case 3:
+	    printf("chose 3\n");
+	    break;
+	    //Post a sale
+	  case 4:
+	    printf("chose 4\n");
+	    break;
+	    //Search for users
+	  case 5:
+	    printf("chose 5\n");
+	    return 0;//temp
+	    break;
+	    //logout and close DB
+	  default:
+	    printf("Error: %d is not an option\n", choice);
+	    break;
+	  }
+	}
+    }
     return 0;
-}
+    }
 
 extern int openDB(char *filename, sqlite3 *db)
 {
