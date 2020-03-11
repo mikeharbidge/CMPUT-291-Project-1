584e5f658eeb852880563e89db8f786bdad78dd6
NickSNFK <61804880+NickSNFK@users.noreply.github.com>
2020-03-11 16:10:32 -0600

    Add files via upload

--- PostSale.c
+++ PostSale.c
@@ -1,2 +1,62 @@
-#CMPUT 291 Mini-Project 1 Harbidge, Parhamglst, Wielgus
-#allows user to post a sale described in spec
+//CMPUT 291 Mini-Project 1 Harbidge, Parhamglst, Wielgus
+//allows user to post a sale described in spec
+#include "PostSale.h"
+
+void Post_Sale(char* userEmail, sqlite3* db){
+  char *zErrMsg = 0;
+  const char* data = "Callback function called";
+
+  char z[50];
+  char sid[50];char pid[50];char text[500];
+  char search[100]; char cond[500];
+  int days=0;
+  int price=0;
+  fgets(z,50,stdin);
+
+  printf("Enter sale_id: ");
+  fgets(sid,50,stdin);
+  if(sid[strlen(sid)-1]=='\n')
+    sid[strlen(sid)-1]='\0';
+  printf("Enter Product_id: ");
+  fgets(pid,50,stdin);
+  if(pid[strlen(pid)-1]=='\n')
+    pid[strlen(pid)-1]='\0';
+  printf("Enter review description: ");
+  fgets(text,500,stdin);
+  if(text[strlen(text)-1]=='\n')
+    text[strlen(text)-1]='\0';
+  printf("Enter condition: ");
+  fgets(cond,500,stdin);
+  if(cond[strlen(cond)-1]=='\n')
+    cond[strlen(cond)-1]='\0';
+  printf("How many days will the sale be up?");
+  scanf("%d",&days);
+  printf("What is the price? (0 if none)");
+  scanf("%d",&price);
+  
+  //insert sale statement
+  sprintf(search, "insert into sales values ('%s', '%s', '%s', datetime('now','+%d days'), '%s', '%s', %d)",sid,userEmail,pid,days,text,cond,price);
+  //printf("SQL: %s\n",search);
+  int rc = sqlite3_exec(db, search,callback,(void*)data, &zErrMsg);
+
+  if( rc != SQLITE_OK )
+    {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    }else{
+    fprintf(stdout, "Operation done successfully\n");
+  }
+}
+
+
+static int callback(void *data, int argc, char **argv, char **aColName)
+{
+  int i;
+  fprintf(stderr, "%s: \n", (const char*)data);
+  for(i = 0; i<argc; i++)
+    {
+      printf("%s = %s\n", aColName[i], argv[i] ? argv[i] : "NULL");
+    }
+  printf("\n");
+  return 0;
+}
