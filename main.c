7ee5adfc4ac85f863cf99a6cb9140abd9af761ac
mikeharbidge <55110294+mikeharbidge@users.noreply.github.com>
2020-03-11 12:40:09 -0600

    work on main, added several things
    Main now will open test.db if no argument is given, SearchSales works, and DB is a global variable

--- main.c
+++ main.c
@@ -4,11 +4,23 @@
 #include "main.h"
 
 int main(int argc, char** argv){
-    //testing for login
-    //NEEDS TO ERROR CHECK DB INIT
-  sqlite3* db;
-  int rc = sqlite3_open(argv[1],&db);
-  if(rc)
+  if (argv[1] != NULL)
+  {
+    printf("Opening database file: %s...\n", argv[1]);
+    int rc = sqlite3_open(argv[1],&db);
+    if(rc)
+    {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return 0;
+    }else
+    {
+      fprintf(stderr, "Opened database successfully\n");
+    }
+  }else
+  {
+    printf("No database argument given. Opening default database.\n");
+    int rc = sqlite3_open("test.db",&db);
+    if(rc)
     {
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       return 0;
@@ -17,59 +29,43 @@ int main(int argc, char** argv){
       fprintf(stderr, "Opened database successfully\n");
       
     }
+  }
   
-    int in = 0; //if login succeeded
-    int choice;//input for choosing functionality
-    in = loginScreen();
-    if (in){
-        printf("***LOGIN SUCCEEDED***\n");
-	do{
-	  printf("1. List Products\n2. Search For Sales\n3. Post a Sale\n4. Search for Users\n5. Logout\nInput: ");
-	  scanf("%d",&choice);
-	  switch(choice){
-	  case 1:
-	    break;
-	    //list products
-	  case 2:
-	    break;
-	    //Search for Sales 
-	  case 3:
-	    break;
-	    //Post a sale
-	  case 4:
-	    Search_Users(userEmail,db);
-	    break;
-	    //Search for users
-	  case 5:
-	    sqlite3_close(db);
-	    return 0;
-	    //logout and close DB
-	  default:
-	    printf("Error: Choice is not an option\n");
-	    break;
-	  }
-	}while(1);
-    }
-    return 0;
-}
-
-
-extern int openDB(char *filename, sqlite3 *db)
-{
-    //wrapper function that opens a database connection with the given filename and sqlite3 object.
-    //returns 0 if failed, 1 if succeeded
-    //to close use standard sqlite3_close()
-    int rc = sqlite3_open(filename, &db);
-    if(rc)
+  int in = 0; //if login succeeded
+  int choice;//input for choosing functionality
+  in = loginScreen();
+  if (in)
+  {
+	  do
     {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        return(0);
-    } else 
-    {
-        fprintf(stderr, "Opened database successfully\n");
-        return 0;
-    }
-    return 0;
+	    printf("1. List Products\n2. Search For Sales\n3. Post a Sale\n4. Search for Users\n5. Logout\nInput: ");
+	    scanf("%d",&choice);
+	    switch(choice){
+	    case 1:
+	      break;
+	      //list products
+	    case 2:
+        searchSales();
+	      break;
+	      //Search for Sales 
+	    case 3:
+	      break;
+        //Post a sale
+      case 4:
+	      Search_Users(userEmail,db);
+	      break;
+	      //Search for users
+	    case 5:
+	      sqlite3_close(db);
+	      return 0;
+	      //logout and close DB
+      default:
+	      printf("Error: Choice is not an option\n");
+        break;
+      }
+	  } while(1);
+  }
+  return 0;
 }
 
 /*static int callback(void *data, int argc, char **argv, char **aColName)
--- main.h
+++ main.h
@@ -4,8 +4,10 @@
 #include <string.h>
 #include "Login.h"
 #include "SearchUsers.h"
+#include "SearchSales.h"
 
 int main();
 
 extern int openDB(char *filename, sqlite3 *db;);
-//static int callback(void *data, int argc, char **argv, char **aColName); //Callback will NOT transfer between files
+sqlite3* db;
+static int callback(void *data, int argc, char **argv, char **aColName); //Callback will NOT transfer between files
